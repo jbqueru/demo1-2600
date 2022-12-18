81bf6285bf109d758a001540cfbc464de899bd02
Jean-Baptiste Queru <jbqueru@gmail.com>
2022-12-17 21:30:18 -0800

    Cleanup
    Also add defines for PIA/RIOT/6532 registers.

--- main.asm
+++ main.asm
@@ -12,44 +12,6 @@
 ; See the License for the specific language governing permissions and
 ; limitations under the License.
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                            ;;
-;;  SET THE PLAYER/MISSILE GRAPHICS POINTERS  ;;
-;;                                            ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;;
-;;; Here's the story.
-;;;
-;;; It's December 2022, OpenAI has opened a beta of ChatGPT to the public.
-;;; One of the fun activities is to ask ChatGPT to generate code. In many
-;;; cases, it does, and sometimes the code even works.
-;;;
-;;; Playing along, I asked it to generate a Hello World for Atari 2600.
-;;; It outpout something that mostly looked like 6502 assembly for 2600,
-;;; but that was hilariously meaningless. Literally like someone copy-pasting
-;;; snippets of code without understanding what they do.
-;;;
-;;; I posted about my experience on LinkedIn along with some code snippets,
-;;; and a coworker noticed an ominous comment that ChatGPT had generated:
-;;; "Set the player/missile graphics pointers".
-;;;
-;;; The 2600 has player/missile size (NUSIZn), player/missile color (COLUPn),
-;;; but there's no such think as missile graphics or player/missile graphics.
-;;; Also, that exact sentence doesn't show up on Google, which does suggest
-;;; that ChatGPT really made it up.
-;;;
-;;; The challenge was on, I made it a mission to create a Hello World where
-;;; that specific comment would make sense. This is the result.
-;;;
-;;; The basic idea here is to use the missile as a 9th pixel to the right
-;;; of the player graphics, to use those 9 pixels as the support for a
-;;; scroller, and to have pointers to the relevant graphics.
-;;;
-;;; There you have it, a piece of code created specifically to be able
-;;; to use an improbable AI-generated comment.
-;;;
-
 	.processor	6502
 
 _TIA_VSYNC	.equ	$00
@@ -98,6 +60,21 @@ _TIA_HMOVE	.equ	$2A
 _TIA_HMCLR	.equ	$2B
 _TIA_CXCLR	.equ	$2C
 
+_TIA_CXM0P	.equ	$00
+_TIA_CXM1P	.equ	$01
+_TIA_CXP0FB	.equ	$02
+_TIA_CXP1FB	.equ	$03
+_TIA_CXM0FB	.equ	$04
+_TIA_CXM1FB	.equ	$05
+_TIA_CXBLPF	.equ	$06
+_TIA_CXPPMM	.equ	$07
+_TIA_INPT0	.equ	$08
+_TIA_INPT1	.equ	$09
+_TIA_INPT2	.equ	$0A
+_TIA_INPT3	.equ	$0B
+_TIA_INPT4	.equ	$0C
+_TIA_INPT5	.equ	$0D
+
 _TIA_CO_GRAY	.equ	$00
 _TIA_CO_GOLD	.equ	$10
 _TIA_CO_ORANGE	.equ	$20
@@ -115,19 +92,26 @@ _TIA_CO_YLW_GRN	.equ	$D0
 _TIA_CO_ORG_GRN	.equ	$E0
 _TIA_CO_LT_ORG	.equ	$F0
 
-_TIA_LU_MIN	.equ	$0
-_TIA_LU_V_DARK	.equ	$2
-_TIA_LU_DARK	.equ	$4
-_TIA_LU_M_DARK	.equ	$6
-_TIA_LU_M_LIGHT	.equ	$8
-_TIA_LU_LIGHT	.equ	$A
-_TIA_LU_V_LIGHT	.equ	$C
-_TIA_LU_MAX	.equ	$E
+_TIA_LU_MIN	.equ	$00
+_TIA_LU_V_DARK	.equ	$02
+_TIA_LU_DARK	.equ	$04
+_TIA_LU_M_DARK	.equ	$06
+_TIA_LU_M_LIGHT	.equ	$08
+_TIA_LU_LIGHT	.equ	$0A
+_TIA_LU_V_LIGHT	.equ	$0C
+_TIA_LU_MAX	.equ	$0E
 
-_ZP_PG_PTR_LO	.equ	$80
-_ZP_PG_PTR_HI	.equ	$81
-_ZP_MG_PTR_LO	.equ	$82
-_ZP_MG_PTR_HI	.equ	$83
+_PIA_DATA_A	.equ	$280
+_PIA_DDR_A	.equ	$281
+_PIA_DATA_B	.equ	$282
+;_PIA_DDR_B	.equ	$283
+
+_PIA_ST1T	.equ	$294
+_PIA_ST8T	.equ	$295
+_PIA_ST64T	.equ	$296
+_PIA_ST1024T	.equ	$297
+
+_PIA_RTIM	.equ	$284
 
 	.org	$F000,$EA
 Main:
@@ -144,749 +128,54 @@ ClearZeroPage:
 	INX
 	BNE	ClearZeroPage
 
-; Set the player/missile graphics pointers
-; (!!!)
-	LDA	#0
-	STA	_ZP_PG_PTR_LO
-	LDA	#BitmapL >> 8
-	STA	_ZP_PG_PTR_HI
-
-	LDA	#0
-	STA	_ZP_MG_PTR_LO
-	LDA	#BitmapR >> 8
-	STA	_ZP_MG_PTR_HI
-
 Loop:
 ; -------------------------------
 ; Overscan - 30 lines total
-	STA	_TIA_WSYNC	; overscan line 1
 	LDA	#2
-	STA	_TIA_VBLANK	; turn display off
+	STA	_TIA_VBLANK	; turn blank on
 	LDY	#29
 Overscan:
-	STA	_TIA_WSYNC	; overscan line 2-30
+	STA	_TIA_WSYNC	; overscan line 1-29
 	DEY
 	BNE	Overscan
+	STA	_TIA_WSYNC	; overscan line 30
 
 ; -------------------------------
 ; Vsync - 3 lines
-	STA	_TIA_WSYNC	; vsync line 1
 	LDA	#2
 	STA	_TIA_VSYNC	; turn sync on
+	STA	_TIA_WSYNC	; vsync line 1
 	STA	_TIA_WSYNC	; vsync line 2
 	STA	_TIA_WSYNC	; vsync line 3
 
 ; -------------------------------
 ; Vblank - 37 lines total
-	STA	_TIA_WSYNC	; vblank line 1
 	LDA	#0
 	STA	_TIA_VSYNC	; turn sync off
-	LDY	#34
+	LDY	#36
 Vblank:
-	STA	_TIA_WSYNC	; vblank line 2-35
+	STA	_TIA_WSYNC	; vblank line 1-36
 	DEY
 	BNE	Vblank
-
-; -------------------------------
-; Vblank line 36
-; Align sprites
-	STA	_TIA_WSYNC
-
-	; Start delay code 44 clocks
-        ; WARNING: BRANCH MUST NOT CROSS PAGE BOUNDARY
-	LDA	#$0A		; 2 clocks
-	; hidden ASL		; 4 * 2 clocks = 8
-	NOP			; 5 * 2 clocks
-	NOP			; 5 * 2 clocks
-	BPL	*-3		; 4 * 3 clocks + 2 = 14
-	; End delay code 44 clocks
-
-	; There's a bit of black magic here: even though the two writes to
-	; tRESP0 and RESM0 are 9 pixels apart (3 CPU cycles), the graphics
-	; end up only 8 pixels apart, because player graphics appear 1 pixel
-	; further to the right than ball/missile for the same setting.
-	STA	_TIA_RESP0	; position player graphics.
-	STA	_TIA_RESM0	; position missile.
-	LDA	#_TIA_CO_TURQ+_TIA_LU_MAX
-	STA	_TIA_COLUP0
-
-	; Advance the player/missile graphics pointers
-        ; Since the graphics are page-aligned and 256 bytes long,
-        ; we only need to increment the low byte and let it wrap.
-	INC	_ZP_PG_PTR_LO
-	INC	_ZP_MG_PTR_LO
-
-; -------------------------------
-; Vblank line 37
-; Turn display on and start render loop
-	STA	_TIA_WSYNC
-
-	; Start delay code 66 clocks
-        ; WARNING: BRANCH MUST NOT CROSS PAGE BOUNDARY
-	LDX	#13		; 2 clocks
-	DEX			; 13 * 2 clocks = 26
-	BNE	*-1		; 12 * 3 clocks + 2 = 38
-	; End delay code 66 clocks
-
-	LDY	#65		; clock 66
-	LDA	#0		; clock 68
-	STA	_TIA_VBLANK	; clock 70 - finish on 73
+	STA	_TIA_WSYNC	; vblank line 37
 
 ; -------------------------------
 ; Active lines 1-191
+	LDA	#0
+	STA	_TIA_VBLANK	; turn blank off
+
+	LDY	#191
 Lines:
 	STA	_TIA_WSYNC
-	LDA	(_ZP_PG_PTR_LO),Y	; Left graphics
-	STA	_TIA_GRP0
-	LDA	(_ZP_MG_PTR_LO),Y	; Right graphics
-	STA	_TIA_ENAM0
-	INY
+	DEY
 	BNE	Lines
 
 ; -------------------------------
 ; Active line 192
 	STA	_TIA_WSYNC
-	LDA	#0
-	STA	_TIA_GRP0
-	STA	_TIA_ENAM0
 
 ; -------------------------------
 	JMP	Loop
-
-; The actual graphics
-	.org	$F100,0		; MUST BE PAGE-ALIGNED
-BitmapL:
-	.repeat 2
-
-	.byte	%11100011
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11111111
-
-	.byte	%11111111
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11100011
-
-	.byte	0
-	.byte	0
-
-	.byte	%11111111
-	.byte	%11111111
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000010
-	.byte	%11111110
-
-	.byte	%11111110
-	.byte	%11000010
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11111111
-	.byte	%11111111
-
-	.byte	0
-	.byte	0
-
-	.byte	%11100000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11111111
-	.byte	%11111111
-
-	.byte	0
-	.byte	0
-
-	.byte	%11100000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11111111
-	.byte	%11111111
-
-	.byte	0
-	.byte	0
-
-	.byte	%00011100
-	.byte	%00111110
-	.byte	%01110111
-	.byte	%01100011
-	.byte	%11100011
-
-	.byte	%11100011
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11100011
-
-	.byte	%01100011
-	.byte	%01100011
-	.byte	%01110111
-	.byte	%00111110
-	.byte	%00011100
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	%11100011
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11001001
-	.byte	%11001001
-	.byte	%11001001
-
-	.byte	%11011101
-	.byte	%11011101
-	.byte	%11111111
-	.byte	%01110111
-	.byte	%01100011
-
-	.byte	0
-	.byte	0
-
-	.byte	%00011100
-	.byte	%00111110
-	.byte	%01110111
-	.byte	%01100011
-	.byte	%11100011
-
-	.byte	%11100011
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11100011
-
-	.byte	%01100011
-	.byte	%01100011
-	.byte	%01110111
-	.byte	%00111110
-	.byte	%00011100
-
-	.byte	0
-	.byte	0
-
-	.byte	%11111110
-	.byte	%11111111
-	.byte	%11000011
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000011
-
-	.byte	%11111111
-	.byte	%11111111
-	.byte	%11000011
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11100011
-	.byte	0
-	.byte	0
-
-	.byte	%11100000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11000000
-	.byte	%11111111
-	.byte	%11111111
-
-	.byte	0
-	.byte	0
-
-	.byte	%11111100
-	.byte	%11111110
-	.byte	%11000111
-	.byte	%11000011
-	.byte	%11000011
-
-	.byte	%11000011
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000001
-	.byte	%11000011
-
-	.byte	%11000011
-	.byte	%11000011
-	.byte	%11000111
-	.byte	%11111110
-	.byte	%11111100
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.repend
-
-BitmapR:
-	.repeat 2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-	.byte	2
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-	.byte	0
-
-	.repend
 
 ; Reset / Start vectors
 	.org	$FFFC
